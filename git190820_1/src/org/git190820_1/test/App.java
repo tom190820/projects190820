982e3eb7ab2c5492c7f1d3315238922589b3e496
tom190820 <59425515+tom190820@users.noreply.github.com>
2020-01-02 16:35:39 +0800

    Update App.java

--- git190820_1/src/org/git190820_1/test/App.java
+++ git190820_1/src/org/git190820_1/test/App.java
@@ -3,7 +3,7 @@ package org.git190820_1.test;
 public class App {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 99; i++) {
+		for (int i = 0; i < 199; i++) {
 			System.out.println("Hello");
 		}
 	}
