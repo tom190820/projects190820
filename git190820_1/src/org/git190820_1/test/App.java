2fc3482e0c74c7ec549a35bad59eb1d835d0d37f
tom190820 <java190820@163.com>
2020-01-02 16:33:05 +0800

    本地的一次编辑，呵呵

--- git190820_1/src/org/git190820_1/test/App.java
+++ git190820_1/src/org/git190820_1/test/App.java
@@ -3,6 +3,8 @@ package org.git190820_1.test;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("ÄãºÃ");
+		for (int i = 0; i < 99; i++) {
+			System.out.println("Hello");
+		}
 	}
 }
